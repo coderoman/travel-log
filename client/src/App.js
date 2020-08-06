c59662f4ebb37a5a1c4056cf0ffab79b21a227d6
Roman <r.reset@gmail.com>
2020-08-06 13:35:09 +0700

    set up mapbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
-import ReactMapGl from "react-map-gl";
+import React, { useState, useEffect } from 'react';
+import ReactMapGl from 'react-map-gl';
 
 const App = () => {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+  const initialViewport = {
     latitude: 37,
     longtitude: -122,
-    zoom: 8,
-  });
+  };
 
-  return <ReactMapGl {...viewport} onViewportChange={setViewport} />;
+  const [viewport, setViewport] = useState(initialViewport);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setViewport({
+          latitude,
+          longitude,
+        });
+      },
+      () => {},
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  }, []);
+
+  return (
+    <ReactMapGl
+      {...viewport}
+      width="100vw"
+      height="100vh"
+      zoom={11}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    />
+  );
 };
 
 export default App;
